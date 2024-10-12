df81b39dfbd71e43de2c19be751452edcfc03ad9
Philip Trauth <philip.trauth@ost.ch>
2024-10-12 10:56:36 +0200

    Added typehints

--- lost_spc/constants/__init__.py
+++ lost_spc/constants/__init__.py
@@ -9,19 +9,21 @@ _D_TABLE = {
     },
 }
 
-def d(m, sim_size=100_000):
+
+def d(m: int, sim_size: int = 100_000) -> float:
     if m < 2:
         raise ValueError("Sample size m has to be >= 2.")
 
     if m in _D_TABLE:
         return _D_TABLE[m]["d2"], _D_TABLE[m]["d3"]
- 
-    X = st.norm.rvs(size=(sim_size, m))
-    R_i = X.max(axis=1) - X.min(axis=1)
+
+    x = st.norm.rvs(size=(sim_size, m))
+    R_i = x.max(axis=1) - x.min(axis=1)
     d2 = np.mean(R_i)
     d3 = np.std(R_i, ddof=1)
     return d2, d3
 
 
-def c4(m):
+def c4(m: int) -> float:
     return gamma(m / 2) / gamma((m - 1) / 2) * np.sqrt(2 / (m - 1))
+
