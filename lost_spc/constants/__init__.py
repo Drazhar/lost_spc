91db170a5cfe4a573de1cd0a93f7631d5d052a08
Philip Trauth <philip.trauth@ost.ch>
2024-10-12 10:54:48 +0200

    constants folder for sub-modules

--- lost_spc/constants/__init__.py
+++ lost_spc/constants/__init__.py
@@ -2,8 +2,20 @@ import numpy as np
 import scipy.stats as st
 from scipy.special import gamma
 
+_D_TABLE = {
+    2: {
+        "d2": 0.7,
+        "d3": 0.8,
+    },
+}
 
-def d(m, sim_size=100000):
+def d(m, sim_size=100_000):
+    if m < 2:
+        raise ValueError("Sample size m has to be >= 2.")
+
+    if m in _D_TABLE:
+        return _D_TABLE[m]["d2"], _D_TABLE[m]["d3"]
+ 
     X = st.norm.rvs(size=(sim_size, m))
     R_i = X.max(axis=1) - X.min(axis=1)
     d2 = np.mean(R_i)
