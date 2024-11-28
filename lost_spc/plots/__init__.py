233fc62dc9268f82aae682bbf8be4ef7a773f097
Philip Trauth <philip.trauth@ost.ch>
2024-11-28 09:31:58 +0100

    Sorted __all__

--- lost_spc/plots/__init__.py
+++ lost_spc/plots/__init__.py
@@ -2,11 +2,11 @@ from .cards import X_R, X_S, R, S
 from .plots import plot_histogram_normal, plot_qq_plot, shewhart_card
 
 __all__ = [
-    "plot_qq_plot",
-    "plot_histogram_normal",
-    "shewhart_card",
     "R",
-    "X_R",
     "S",
+    "X_R",
     "X_S",
+    "plot_histogram_normal",
+    "plot_qq_plot",
+    "shewhart_card",
 ]
